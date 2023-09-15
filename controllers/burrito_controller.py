1b97c32c571e33566a7b525d2d9d719af2a50fc2
Neil Roberts <neilroberts123@hotmail.co.uk>
2023-09-15 18:49:53 +0100

    started routes and htmls

--- controllers/burrito_controller.py
+++ controllers/burrito_controller.py
@@ -8,11 +8,28 @@ burrito_blueprint = Blueprint("burritos", __name__)
 
 @burrito_blueprint.route("/home")
 def home():
-
     return render_template("/home.jinja")
 
 @burrito_blueprint.route("/burritos")
 def show_all_burritos():
-
     all_burritos = Burrito.query.all()
     return render_template("/burritos.jinja", burritos=all_burritos)
+
+@burrito_blueprint.route("/burritos/<id>")
+def show_one_burrito(id):
+    burrito_to_show = Burrito.query.all(id)
+    return render_template("/show_one_burrito.jinja", burrito=burrito_to_show)
+
+@burrito_blueprint.route("/burritos", methods = ["POST"])
+def add_burrito():
+    burrito_name = request.form["name"]
+    burrito_price = request.form["price"]
+    
+    burrito_to_be_added = Burrito(name=burrito_name, price=burrito_price)
+
+    db.session.add(burrito_to_be_added)
+    db.session.commit()
+
+    return redirect ("/burritos")
+
+
--- controllers/order_controller.py
+++ controllers/order_controller.py
@@ -1,5 +1,18 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
+from models.burrito import Burrito
+from models.burrito_order import Burrito_order
+from models.order import Order
 from app import db
 
 order_blueprint = Blueprint("orders", __name__)
+
+@order_blueprint.route("/orders")
+def show_all_orders():
+    all_orders = Order.query.all()
+    return render_template("/orders.jinja", orders=all_orders)
+
+@order_blueprint.route("/orders/<id>")
+def show_one_order(id):
+    one_order = Order.query.all(id)
+    return render_template("/show_one_burrito.jinja", order=one_order)
