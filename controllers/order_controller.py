1da13c4df458e801f26f8e59dfd0735054e630be
Neil Roberts <neilroberts123@hotmail.co.uk>
2023-09-17 23:18:13 +0100

    working through methods

--- controllers/order_controller.py
+++ controllers/order_controller.py
@@ -7,6 +7,18 @@ from app import db
 
 order_blueprint = Blueprint("orders", __name__)
 
+@order_blueprint.route("/orders")
+def show_all_orders():
+    all_orders = Order.query.all()
+    return render_template("/orders.jinja", orders=all_orders)
+
+@order_blueprint.route("/orders<id>")
+def show_order(id):
+    order_to_show = Order.query.get(id)
+    return render_template("show_order.jinja", order=order_to_show)
+
+
+
 # @order_blueprint.route("/create_order", method=["POST"])
 # def new_order():
 #     burritos = Burrito.query.all()
