310a04986485d1d7ed7e101f34e4731457752a55
Neil Roberts <neilroberts123@hotmail.co.uk>
2023-09-18 20:21:31 +0100

    working on delete functions

--- controllers/order_controller.py
+++ controllers/order_controller.py
@@ -19,25 +19,39 @@ def show_order(id):
     return render_template("show_order.jinja", order=order_to_show, burritos=burritos)
 
 @order_blueprint.route("/orders/<id>", methods=["POST"])
-def add_to_order(id):
-    burritos = Burrito.query.all()
+def add_burritos_to_order(id):
     
+        burritos = Burrito.query.all()
+        for burrito in burritos:
+            quantity = request.form.get(str(burrito.id))
+            if quantity and int(quantity) > 0:
+                existing_burrito_order = Burrito_order.query.filter_by(burrito_id=burrito.id, order_id=id).first()
+                if existing_burrito_order:
+                    existing_burrito_order.quantity += int(quantity)
+                else:
+                    burrito_order = Burrito_order(burrito_id=burrito.id, order_id=id, quantity=quantity)
+                    db.session.add(burrito_order)
 
-    for burrito in burritos:
-        quantity = request.form[str(burrito.id)]
-        if int(quantity) > 0:
-            burrito_order = Burrito_order(burrito_id=int(burrito.id), order_id=id, quantity=quantity)
-            db.session.add(burrito_order)
-            db.session.commit()
-
-    id_for_path = id
-    return redirect (f"/orders/{id_for_path}")
-    
+        db.session.commit()
+        return redirect (f"/orders/{id}")
 
 
 
 
-# @order_blueprint.route("/create_order", method=["POST"])
-# def new_order():
+
+
+
+# def add_to_order(id):
 #     burritos = Burrito.query.all()
-#     orders = Order.query.all()
+    
+
+#     for burrito in burritos:
+#         quantity = request.form[str(burrito.id)]
+#         if int(quantity) > 0:
+#             burrito_order = Burrito_order(burrito_id=int(burrito.id), order_id=id, quantity=quantity)
+#             db.session.add(burrito_order)
+#             db.session.commit()
+
+#     id_for_path = id
+#     return redirect (f"/orders/{id_for_path}")
+    
